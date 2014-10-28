7d9acca93af69656f062fa8474542022a72e30a9
Mark Ableidinger <mark.ableidinger@gmail.com>
2014-10-28 13:04:44 -0700

    updates templates and static files

--- static/login.js
+++ static/login.js
@@ -1,7 +1,33 @@
-var passwordBox = $('#password')
+$(document).ready(function(){
+var emailBox = $('#email')
 var loginButton = $('#login-button')
-var toggle = $('#register-toggle')
+var toggleLink = $('#register-toggle')
+var toggleVar = $('#toggle')
 
 function registrify() {
-    
+    emailBox.html("<input placeholder='Email' class='email-box' type='text' name='email' />")
+    loginButton.attr('value', 'Register')
+    toggleVar.attr('value', 'register')
+    toggleLink.html('<a href=#>Login</a>')
 }
+
+function loginify() {
+    emailBox.html('')
+    loginButton.attr('value', 'Login')
+    toggleVar.attr('value', 'login')
+    toggleLink.html('<a href=#>Don\'t have an account? Sign up now</a>')
+}
+
+function togglify() {
+    if(toggleVar.val() == 'login'){
+        registrify()
+    }
+    else{
+        loginify()
+    }
+}
+
+toggleLink.on('click', function(){togglify()})
+
+
+})
